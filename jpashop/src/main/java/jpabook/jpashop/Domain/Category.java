7bb5d8863db14b4e061a594c749caebd646d5742
White-Asher <jaeyoon.han97@gmail.com>
2022-12-22 01:39:20 +0900

    Study: JPA1-엔티티 설계시 주의점 추가

--- jpashop/src/main/java/jpabook/jpashop/Domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/Domain/Category.java
@@ -22,16 +22,18 @@ public class Category {
     @ManyToMany
     @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category"),
-            inverseJoinColumns = @JoinColumn(name = "item_id")
-    )
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
-
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
