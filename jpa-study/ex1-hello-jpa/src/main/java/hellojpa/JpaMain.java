4092939f9d39b33295998abef03751c8ab812d4e
White-Asher <jaeyoon.han97@gmail.com>
2022-12-31 23:20:58 +0900

    Study: JPA0-Hello JPA - 애플리케이션 개발

--- jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,9 +15,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            for (Member member: result) {
+                System.out.println("member = " + member.getName());
+            }
+            // 정상적으로 종료하면 commit
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
