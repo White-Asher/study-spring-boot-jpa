f3f74ee5b75edc8950238a27e10b397378a84ce0
White-Asher <jaeyoon.han97@gmail.com>
2023-01-03 19:59:35 +0900

    Study: JPA0-temp

--- jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,19 +15,25 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            for (Member member: result) {
-                System.out.println("member = " + member.getName());
-            }
-            // 정상적으로 종료하면 commit
+            // 150L의 member는 영속상태가 된다.
+            Member member = em.find(Member.class, 150L);
+            // 값이 변경되어 Dirty Checking 이 일어난다.
+            member.setName("AAAAA");
+
+            // EntityManager 내의 영속성 컨택스트 전체가 지워진다.
+            em.close();
+            System.out.println("=== AFTER em.close() ===");
+
+            // 150L의 member를 다시 조회해도 영속성 컨택스트가 닫혀서 저장할 곳이 없다.
+            Member member2 = em.find(Member.class, 150L);
+
+            System.out.println("=== BEFORE commit ===");
             tx.commit();
         } catch (Exception e) {
+            // 비정상 종료시 rollback
             tx.rollback();
         } finally {
+            // EntityManager는 쓰레드간 공유해선 안되므로 사용을 다하면 꼭 닫아줘야 한다.
             em.close();
         }
         emf.close();
