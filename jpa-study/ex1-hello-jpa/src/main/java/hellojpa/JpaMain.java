fa56fdaf9895a6bc2ae92c0aac266dd717146677
White-Asher <jaeyoon.han97@gmail.com>
2023-01-11 00:27:05 +0900

    Study: jpa

--- jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,19 +15,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 150L의 member는 영속상태가 된다.
-            Member member = em.find(Member.class, 150L);
-            // 값이 변경되어 Dirty Checking 이 일어난다.
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
-            // EntityManager 내의 영속성 컨택스트 전체가 지워진다.
-            em.close();
-            System.out.println("=== AFTER em.close() ===");
+            Team team = new Team();
+            team.setName("teamA");
+            team.getMembers().add(member);
+            em.persist(team);
 
-            // 150L의 member를 다시 조회해도 영속성 컨택스트가 닫혀서 저장할 곳이 없다.
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("=== BEFORE commit ===");
             tx.commit();
         } catch (Exception e) {
             // 비정상 종료시 rollback
--- jpa-study/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-study/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,40 +1,41 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 //@Table(name = "USER")
 public class Member {
 
     @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
     private Long id;
     //@Column(name = "username")
-    private String name;
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getUsername() {
+        return username;
     }
 
-    public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
