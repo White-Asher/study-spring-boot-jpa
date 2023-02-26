d2a909dd8b5eec91bbf507d7480b33c8a70bedc6
White-Asher <dkxmpower999@naver.com>
2023-02-26 17:10:37 +0900

    Study: JPA2 실습 코드 추가

---
+++ jpashop-v2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.Domain.Order;
+import jpabook.jpashop.Domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * x To One (ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 엔티티를 직접 노출했을 때 발생하는 문제...
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
---
+++ jpashop-v2/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.Domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    // 가까운 Transactional 어노테이션이 readOnly=true를 오버라이드함 (기본값은 false)
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+}
